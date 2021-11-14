0aee6ac0f58f7473957d95c89ce1cc9dea9331a4
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-14 14:09:44 +0530

    Added : email & validation check

--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * The UserRegistration program implements an application that
+ * The UserRegistration program implements an application that take user details with proper pattern or condition
  * @author Antara
  * @since 2021-11-12
  */
 
 public class UserRegistration {
+    Scanner user_input = new Scanner(System.in);
     public void firstName(){
         /**
          *This method is used to add First Name with proper pattern
          */
-        Scanner user_input = new Scanner(System.in);
         System.out.println("Enter the First Name");
         String fName=user_input.nextLine();
         boolean checkingCondition = Pattern.matches("[A-Z][a-z]{3,}", fName);
@@ -29,7 +29,6 @@ public class UserRegistration {
         /**
          *This method is used to add Last Name with proper pattern
          */
-        Scanner user_input = new Scanner(System.in);
         System.out.println("Enter the Last Name");
         String lName=user_input.nextLine();
         boolean checkingCondition = Pattern.matches("[A-Z][a-z]{3,}", lName);
@@ -39,14 +38,28 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid Last name which start with Capital letter" +
                     " and has minimum 3 characters");
     }
+
+    public void email(){
+        /**
+         *This method is used to add Email with proper pattern
+         */
+        System.out.println("Enter the Email Id");
+        String email=user_input.nextLine();
+        boolean checkingCondition = Pattern.matches("^^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*$", email);
+        if (checkingCondition==true)
+            System.out.println("Email Id : "+email);
+        else
+            System.out.println("Please Enter a Valid Email Id");
+    }
     public static void main(String[] args) {
         /**
-         * This is main method which makes use of firstName, lastName methods
+         * This is main method which makes use of firstName, lastName, email methods
          * @return nothing
          */
         //System.out.println("Welcome To User Registration Problem");
         UserRegistration userRegistration=new UserRegistration();
         userRegistration.firstName();
         userRegistration.lastName();
+        userRegistration.email();
     }
 }
