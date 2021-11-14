916117560285c370c44d0501d775191dc13012cf
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-14 12:31:13 +0530

    Added : first name & validation check

--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -1,5 +1,8 @@
 package com.bridgelab;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  * The UserRegistration program implements an application that
  * @author Antara
@@ -7,7 +10,20 @@ package com.bridgelab;
  */
 
 public class UserRegistration {
+    public void firstName(){
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Enter the First Name");
+        String fName=user_input.nextLine();
+        boolean checkingCondition = Pattern.matches("[A-Z][a-z]{3,}", fName);
+        if (checkingCondition==true)
+            System.out.println("First Name : "+fName);
+        else
+            System.out.println("Please Enter a Valid First name which start with Capital letter" +
+                    " and has minimum 3 characters");
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To User Registration Problem");
+        //System.out.println("Welcome To User Registration Problem");
+        UserRegistration userRegistration=new UserRegistration();
+        userRegistration.firstName();
     }
 }
