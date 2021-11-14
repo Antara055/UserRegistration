76ea65262e4a056a2064f95ee997f34d2a414447
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-14 12:48:30 +0530

    Added : last name & validation check

--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -11,6 +11,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public void firstName(){
+        /**
+         *This method is used to add First Name with proper pattern
+         */
         Scanner user_input = new Scanner(System.in);
         System.out.println("Enter the First Name");
         String fName=user_input.nextLine();
@@ -21,9 +24,29 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid First name which start with Capital letter" +
                     " and has minimum 3 characters");
     }
+
+    public void lastName(){
+        /**
+         *This method is used to add Last Name with proper pattern
+         */
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Enter the Last Name");
+        String lName=user_input.nextLine();
+        boolean checkingCondition = Pattern.matches("[A-Z][a-z]{3,}", lName);
+        if (checkingCondition==true)
+            System.out.println("Last Name : "+lName);
+        else
+            System.out.println("Please Enter a Valid Last name which start with Capital letter" +
+                    " and has minimum 3 characters");
+    }
     public static void main(String[] args) {
+        /**
+         * This is main method which makes use of firstName, lastName methods
+         * @return nothing
+         */
         //System.out.println("Welcome To User Registration Problem");
         UserRegistration userRegistration=new UserRegistration();
         userRegistration.firstName();
+        userRegistration.lastName();
     }
 }
