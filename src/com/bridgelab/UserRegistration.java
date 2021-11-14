65fb28831a70ea660f3ad2c93f1f778c284d68eb
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-14 12:10:02 +0530

    Added : Welcome to User Registration Problem

---
+++ src/com/bridgelab/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelab;
+
+/**
+ * The UserRegistration program implements an application that
+ * @author Antara
+ * @since 2021-11-12
+ */
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration Problem");
+    }
+}
